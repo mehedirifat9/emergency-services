a16ee4cbfaa7871db2d9527e3c40a47975dccd07
mehedirifat9 <mehedirifat9@gmail.com>
2025-08-27 02:22:27 +0600

    Heart count completed

---
+++ script.js
@@ -0,0 +1,13 @@
+const heartIcon = document.getElementsByClassName("heartIcon");
+for (const Icon of heartIcon){
+    let heartNumber = 0;
+    Icon.addEventListener('click', function(){
+        // console.log("Heart Clicked");
+        const initialHeart = parseInt(document.getElementById('initial-heart').innerText);
+        // console.log(initialHeart);
+        heartNumber = 0 + 1;
+        let finalHeartCount = initialHeart + heartNumber;
+        console.log(finalHeartCount);
+        document.getElementById("initial-heart").innerText = finalHeartCount;        
+    }, { once: true })
+}
